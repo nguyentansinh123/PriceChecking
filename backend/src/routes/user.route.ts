6d2e6383b4d1643f7675bc07d28ece9cc524b0ef
sinh <nsinh2281@gmail.com>
2025-07-29 11:55:16 +1000

    some user functionalities

--- backend/src/routes/user.route.ts
+++ backend/src/routes/user.route.ts
@@ -1,11 +1,13 @@
 import express from 'express'
 import { deleteUser, editMyAvatar, getAllUsersDetails, getMyDetails } from '../controller/user.controller'
+import { userAuth } from '../middleware/userAuth'
+import { upload } from '../middleware/multer'
 
 export const router = express.Router()
 
 
-router.get('/get-my-details',getMyDetails)
+router.get('/get-my-details',userAuth,getMyDetails)
 router.get('/get-all-user-details',getAllUsersDetails)
-router.put('/edit-avatar',editMyAvatar)
-router.delete('/delete-user',deleteUser)
+router.put('/edit-avatar',userAuth,upload.single('avatar'),editMyAvatar)
+router.delete('/delete-user',userAuth,deleteUser)
 
